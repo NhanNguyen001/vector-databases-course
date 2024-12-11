c21abe37c916fa57aede79ab8e6a02151893ff19
Nhan Nguyen <nhan.nguyen@27global.com>
2024-12-11 13:34:52 +0700

    Finish vector_db_llm_lang_chain.py

--- self-learning/vector_db_llm_lang_chain.py
+++ self-learning/vector_db_llm_lang_chain.py
@@ -7,8 +7,28 @@ from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 
 load_dotenv()
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
 
 openai_key = os.getenv("OPENAI_API_KEY")
 
@@ -32,8 +52,19 @@ print(f"Number of documents: {len(documents)}")
 embedding = OpenAIEmbeddings(api_key=openai_key, model="text-embedding-3-small")
 
 persist_directory = "./db/chroma_db_real_world"
+
 vectordb = Chroma.from_documents(
     documents=documents, embedding=embedding, persist_directory=persist_directory
-)
+)  # This will create a vector database in the specified directory
 
-print(vectordb)
+# Now we can use the vector database to answer questions
+retriever = vectordb.as_retriever()
+
+question_answer_chain = create_stuff_documents_chain(llm=model, prompt=prompt)
+
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+response = rag_chain.invoke({"input": "Talk about databricks new?"})
+res = response["answer"]
+
+print(res)
